66944785bae02f941e00665d2de84d9351e39f6d
Alex Beltyukov <beltyukov.1@gmail.com>
2014-10-12 12:57:55 -0500

    Add upload file input

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,36 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring"),
+    fs = require("fs");
 
-function start(res) {
+function start(res, postData) {
   console.log("Request handler 'start' was called.");
 
-  exec("find /", { timeout: 10000, maxBuffer: 20000*1024 },
-    function (error, stdout, stderr) {
-      res.writeHead(200, {"Content-Type": "text/plain"});
-      res.end(stdout);
-    });
+  var body = '<html>' +
+    '<head>' +
+    '<meta http=equiv="Content-Type" content="text/html"; ' +
+    'charset=UTF-8 />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+    '<input type="file" name="upload" />' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end(body);
 }
 
-function upload(res) {
+function upload(res, postData) {
   console.log("Request handler 'upload' was called.");
   res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello Upload!");
+  res.end("You've sent: " + querystring.parse(postData).text);
+}
+
+function show(res) {
+  console.log("Request handler 'show' was called.");
+  res.writeHead(200, {"Content-Type": "image/png"});
+  fs.createReadStream("/tmp/test.png").pipe(res);
 }
 
 exports.start = start;
